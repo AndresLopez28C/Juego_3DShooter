43ce87aab4d0b7609a2354d15051d0257cb36d51
Andres <arlopeza@udistrital.edu.co>
2025-12-26 16:49:59 -0500

    ahora persigue al jugador

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,5 +1,11 @@
 extends RigidBody3D
 @onready var bat_model: Node3D = %bat_model
-
+@onready var player = get_node("/root/Game/Player")
+const speed = 3.0
 func _take_damage():
 	bat_model._hurt()
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	direction.y=0.0
+	linear_velocity = direction * speed
+	bat_model.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP)
